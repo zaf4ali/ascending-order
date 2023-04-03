b6c61b2b7a022652147f87e649af318dd2841f85
Md Zafar <72871013+zaf4ali@users.noreply.github.com>
2023-04-03 16:05:11 +0530

    Update assesment.py

--- assesment.py
+++ assesment.py
@@ -7,6 +7,6 @@ folder_path = "output"
 files = os.listdir(folder_path)
 
 
-files = sorted(files, key=lambda f: (os.path.join(folder_path, f))).......
+files = sorted(files, key=lambda f: (os.path.join(folder_path, f)))
 
 print(files)
